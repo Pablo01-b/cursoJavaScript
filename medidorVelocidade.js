c5408cf5cd721691775d33828c0ef7274d303959
Pablo Bruci <pablobrucipb@gmail.com>
2023-09-28 15:36:19 -0300

    Mini Projeto 01: Medidor de Velocidade

--- medidorVelocidade.js
+++ medidorVelocidade.js
@@ -1,8 +1,8 @@
 //MINI PROJETO 01: MEDIDOR DE VELOCIDADE.
 //REQUISITOS:
-//1- A velocidade máxima permitida é de até 70 km/h
-//2- A cada 5km acima do limite você ganha 1 ponto na carteira
-//3- caso os pontos >= 12: mostrar a mensagem: "Carteira suspendida"
+//1-A velocidade máxima permitida é de até 70 km/h
+//2-A cada 5km acima do limite você ganha 1 ponto na carteira
+//3-caso os pontos >= 12: mostrar a mensagem: "Carteira suspendida"
 
 verificarVelocidade(130);
 
