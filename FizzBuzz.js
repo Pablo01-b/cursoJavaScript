72d9ef797dfc3a34419894aa8836b608905d0b93
Pablo Bruci <pablobrucipb@gmail.com>
2023-09-28 15:45:23 -0300

    Mini Projeto 01: FIZZBUZZ

--- FizzBuzz.js
+++ FizzBuzz.js
@@ -1,23 +1,23 @@
 //MINI PROJETO 02: FIZZBUZZ
-//Divisível por 3 = Fizz
-//Divisível por 5 = Buzz
-//Divisível por 3 e 5 = FizzBuzz
-//Não é é divisível por 3 ou 5 = entrada
-//Se o valor não é um número = "Não é um número"
+//Divisível por 3: Fizz
+//Divisível por 5: Buzz
+//Divisível por 3 e 5: FizzBuzz
+//Não é é divisível por 3 ou 5: entrada
+//Se o valor digitado não for um número mostre a mensagem: "Não é um número"
 
-const resultado = fizzBuzz(3);
+const resultado = fizzBuzz(20);
 console.log(resultado);
 
 function fizzBuzz(entrada){
     if(typeof entrada !== 'number'){
-        console.log("Não é um número");
+        return "Não é um número";
     }else if(entrada % 3 === 0 && entrada % 5 === 0){
-        console.log("FizzBuzz");
+        return "FizzBuzz";
     }else if(entrada % 3 !== 0 && entrada % 5 !== 0){
-        console.log(entrada);
+        return entrada;
     }else if(entrada % 3 === 0){
-        console.log("Fizz");
+        return "Fizz";
     }else if(entrada % 5 === 0){
-        console.log("Buzz");
+        return "Buzz";
     }
 }
