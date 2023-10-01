bcfa73be6e9602e1822b26e7ea6d3bf0cf42e56c
Pablo Bruci <pablobrucipb@gmail.com>
2023-10-01 12:20:35 -0300

    Mini Projeto: Exibir Asteriscos

---
+++ ExibirAsteriscos.js
@@ -0,0 +1,12 @@
+//MINI PROJETO: EXIBIR ASTERISCOS
+//Criar uma função que exibe a quantidade de *
+
+exibirAsteriscos(10);
+
+function exibirAsteriscos(linhas){
+   let padrao = '';
+   for(let linha = 1; linha <= linhas; linha++){
+    padrao += '*';
+    console.log(padrao);
+   }
+}
