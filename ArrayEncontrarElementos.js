be2a2ca3c6adf6cc3692b0e0282869758363e2c9
Pablo Bruci <pablobrucipb@gmail.com>
2023-10-24 17:57:58 -0300

    Encontrando elementos no Array

---
+++ ArrayEncontrarElementos.js
@@ -0,0 +1,11 @@
+//ENCONTRANDO ELEMENTOS (PRIMITIVOS)
+
+/*const numeros = [1,2,3,4,1];
+console.log(numeros.indexOf(5)); //retorna o indice ou -1 caso não encontre
+console.log(numeros.lastIndexOf(1)); //retorna a ultima ocorrência do número informado na posição do array*/
+
+/*const numeros = [1,2,3,4,5,6,];
+console.log(numeros.indexOf(2) !== -1);*/
+
+const numeros = [1,2,3,4,5,6,7,8];
+console.log(numeros.includes(9)); //retorna se o array tem o elemento informado.
